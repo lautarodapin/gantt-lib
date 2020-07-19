92910a5a6d111f9d3428a9c3bdec39c46f7c93ad
Lautaro Dapino <lautarodapin@gmail.com>
2020-07-19 17:52:24 -0300

    change version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='gantt-lib-lautarodapin',
-    version='0.0.2',
+    version='0.0.3',
     url="https://github.com/lautarodapin/gantt-lib",
     author="Dapino Lautaro",
     author_email="lautarodapin@gmail.com",
